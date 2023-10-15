90a66768b9a9323ac73d0e8688ce67a28b620ed2
arjundvn24 <adhawan_be20@thapar.edu>
2023-10-15 17:51:40 +0530

    feat: edit html via extension

--- content-script.js
+++ content-script.js
@@ -1,3 +1,4 @@
+// Create a div element for the box
 const box = document.createElement("div");
 box.style.width = "100px";
 box.style.height = "100px";
@@ -7,7 +8,14 @@ box.style.backgroundColor = "blue";
 const productTitleElement = document.getElementById("productTitle");
 
 if (productTitleElement) {
+  // Append the box to the "productTitle" element
   productTitleElement.appendChild(box);
+
+  // Change the background color of the box
+  box.style.backgroundColor = "green";
+
+  // Change the text content of the box
+  box.textContent = "PRODUCT TITLE\nProduct Index";
 } else {
   console.log("Element with ID 'productTitle' not found.");
 }
